6aae9411eb98496856597af6c764e9565ecf6501
LuizRamos <luizramospe@hotmail.com>
2019-05-08 20:37:53 -0400

    FirstCommit

---
+++ EnteringAHouse/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  EnteringAHouse
+//
+//  Created by Luiz on 5/8/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
